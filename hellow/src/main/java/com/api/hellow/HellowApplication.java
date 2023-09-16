713bddecd66aa97e54216ec7f2a53898c8f449c3
taiki-sakashita <minashi.business@gmail.com>
2023-09-16 14:02:06 +0900

    【テストプロジェクト】Add:新しいmavenプロジェクトhellowをルートに作成
    Add:getUser.javaを追加

---
+++ hellow/src/main/java/com/api/hellow/HellowApplication.java
@@ -0,0 +1,13 @@
+package com.api.hellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowApplication.class, args);
+	}
+
+}
---
+++ hellow/src/test/java/com/api/hellow/HellowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
