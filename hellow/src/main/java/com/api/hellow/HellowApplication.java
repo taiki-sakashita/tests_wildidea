7f62096ef8ca4e701a14465575cb60ab5ac5a712
taiki-sakashita <minashi.business@gmail.com>
2023-09-17 12:53:21 +0900

    [テストプロジェクト]Mod:scanBasePackage=com.apiを追記

--- hellow/src/main/java/com/api/hellow/HellowApplication.java
+++ hellow/src/main/java/com/api/hellow/HellowApplication.java
@@ -3,7 +3,7 @@ package com.api.hellow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.api" })
 public class HellowApplication {
 
 	public static void main(String[] args) {
