b3ec048acddd36147ede72f2da242a8f0c6b844e
taiki-sakashita <minashi.business@gmail.com>
2023-09-19 13:40:09 +0900

    【テストプロジェクト】Mod:Jsonを更新しつつ動的なレスポンスが出来るようにした

--- hellow/src/main/java/com/api/controller/GetUser.java
+++ hellow/src/main/java/com/api/controller/GetUser.java
@@ -7,20 +7,37 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.model.UserResponse;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 @RequestMapping("/getUser")
 public class GetUser {
+  private final ObjectMapper mapper = new ObjectMapper();
+  private final Resource resource = new ClassPathResource("stub/user.json");
+
   @GetMapping("/hello")
-  public UserResponse hello() throws IOException {
-    ObjectMapper mapper = new ObjectMapper();
-    Resource resource = new ClassPathResource("stub/user.json");
+  public UserResponse hello(
+      @RequestParam(name = "name", defaultValue = "Alice") String name,
+      @RequestParam(name = "age", defaultValue = "26") int age) throws IOException {
+
     InputStream is = resource.getInputStream();
-    return mapper.readValue(is, UserResponse.class);
+    JsonNode jsonNode = mapper.readTree(is);
+
+    // クエリパラメータから値を取得して jsonNode を更新
+    // ageOfCE(AgeOfContractExpirationの略)はageの値＋30として更新
+    ((ObjectNode) jsonNode).put("name", name);
+    ((ObjectNode) jsonNode).put("age", age);
+    ((ObjectNode) jsonNode).put("ageOfCE", age + 30);
+
+    UserResponse userResponse = mapper.treeToValue(jsonNode, UserResponse.class);
+
+    return userResponse;
   }
 
 }
--- hellow/src/main/java/com/api/model/UserResponse.java
+++ hellow/src/main/java/com/api/model/UserResponse.java
@@ -3,14 +3,16 @@ package com.api.model;
 public class UserResponse {
   private String name;
   private int age;
+  private int ageOfCE;
 
   public UserResponse() {
 
   }
 
-  public UserResponse(String name, int age) {
+  public UserResponse(String name, int age, int ageOfCE) {
     this.name = name;
     this.age = age;
+    this.ageOfCE = age + 30;
   }
 
   public String getName() {
@@ -28,4 +30,14 @@ public class UserResponse {
   public void setAge(int age) {
     this.age = age;
   }
+
+  public int getAgeOfCE() {
+    return ageOfCE;
+  }
+
+  public void setAgeOfCE(int ageOfCE) {
+    this.ageOfCE = ageOfCE;
+    
+  }
+
 }
