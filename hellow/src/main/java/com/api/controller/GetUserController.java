c027842d5050e7703b611fa328b16c902ae4cf6d
taiki-sakashita <minashi.business@gmail.com>
2023-09-23 14:36:48 +0900

    【テストプロジェクト】Fix:指摘を反映

--- hellow/src/main/java/com/api/controller/GetUserController.java
+++ hellow/src/main/java/com/api/controller/GetUserController.java
@@ -2,7 +2,10 @@ package com.api.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.ConsoleHandler;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,26 +21,32 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 @RequestMapping("/getUser")
-public class GetUser {
+public class GetUserController {
   private final ObjectMapper mapper = new ObjectMapper();
   private final Resource resource = new ClassPathResource("stub/user.json");
 
+  private static final Logger logger = LoggerFactory.getLogger(GetUserController.class);
+
   @GetMapping("/hello")
+
   public UserResponse hello(
       @RequestParam(name = "name", required = false) String name,
       @RequestParam(name = "age", required = false) Integer age,
       @RequestParam(name = "membership", required = false) Membership membership) throws IOException {
-    System.out.println(membership);
-    System.out.println(name);
-
-    InputStream is = resource.getInputStream();
-    JsonNode jsonNode = mapper.readTree(is);
+    logger.info("名前:", name);
+    logger.info("メンバーシップ:", membership);
+    JsonNode jsonNode = Try();
 
     updateJsonNode(jsonNode, name, age, membership);
 
-    UserResponse userResponse = mapper.treeToValue(jsonNode, UserResponse.class);
+    return mapper.treeToValue(jsonNode, UserResponse.class);
 
-    return userResponse;
+  }
+
+  private JsonNode Try() throws IOException {
+    try (InputStream is = resource.getInputStream()) {
+      return mapper.readTree(is);
+    }
   }
 
   private void updateJsonNode(JsonNode jsonNode, String name, Integer age, Membership membership) {
@@ -46,8 +55,6 @@ public class GetUser {
     }
     if (age != null) {
       ((ObjectNode) jsonNode).put("age", age);
-    }
-    if (age != null) {
       ((ObjectNode) jsonNode).put("ageOfCE", age.intValue() + 30);
     }
     if (membership != null) {
